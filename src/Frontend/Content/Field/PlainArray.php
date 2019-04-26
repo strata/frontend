505f962cbb0e06e4bd56a9161e689ca8794ae639
Marie Manandise <marie@studio24.net>
2019-04-26 11:07:01 +0100

    Plain array field type and tests

---
+++ src/Frontend/Content/Field/PlainArray.php
@@ -0,0 +1,83 @@
+<?php
+declare(strict_types=1);
+
+namespace Studio24\Frontend\Content\Field;
+
+/**
+ * Array field, keys can be integers or strings, values can only be numeric of strings (no arrays or objects)
+ *
+ * @package Studio24\Frontend\Content\Field
+ */
+class PlainArray extends ContentField
+{
+    const TYPE = 'plainarray';
+
+    protected $content = array();
+
+    /**
+     * Create plain array content field
+     *
+     * @param string $name
+     * @param array $content
+     *
+     * @throws \Studio24\Frontend\Exception\ContentFieldException
+     */
+    public function __construct(string $name, array $content)
+    {
+        $this->setName($name);
+        $this->setContent($content);
+    }
+
+    /**
+     * Set content
+     *
+     * @param array $content
+     * @return PlainArray
+     */
+    public function setContent(array $content) : PlainArray
+    {
+        if (!is_array($content)) {
+            return $this;
+        }
+
+        if (empty($content)) {
+            return $this;
+        }
+
+        foreach ($content as $key => $value) {
+            if (!(is_int($key) || is_string($key))) {
+                continue;
+            }
+
+            if (!(is_numeric($value) || is_string($value))) {
+                continue;
+            }
+
+            $new_key = (is_numeric($key))? (int) $key: (string) $key;
+
+            $this->content[$new_key] = $value;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Return content
+     *
+     * @return array
+     */
+    public function getValue(): array
+    {
+        return $this->content;
+    }
+
+    /**
+     * Return string representation of content field
+     *
+     * @return string
+     */
+    public function __toString() : string
+    {
+        return (string) $this->content;
+    }
+}
